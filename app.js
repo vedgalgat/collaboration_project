9011dbf7a690c8839a309484f5d0d9f0f78d6e03
ved galgat <vedgalgat8233@gmail.com>
2024-08-06 20:37:23 -0700

    second commmit

---
+++ app.js
@@ -0,0 +1,9 @@
+// let form1=document.querySelector("#form1")
+// let form2=document.querySelector("#form2")
+// let feild=document.querySelector(".feild")
+
+// feild.addEventListener("cllck",()=>{
+//     form1.style.display="none "
+// })
+
+
